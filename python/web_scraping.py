ce3b9b7444ad9030eb5ee37b7515030dd79a7ecd
Bastien <bastien.robert91@gmail.com>
2020-11-05 15:46:07 +0100

    web_scraping v2

--- python/web_scraping.py
+++ python/web_scraping.py
@@ -36,6 +36,11 @@ first_paragraph_id2 = soup.p.get("id") # Returns None if no 'id' found
 
 print(first_paragraph_id)
 
+print("**********")
+second_paragraph = [p.text for p in soup("p") if p.get("id") == "subject"]
+second_paragraph_t = soup.find(id="subject").text
+print(second_paragraph)
+print(second_paragraph_t)
 
 all_paragraph = soup.find_all('p') # or soup("p")
 paragraph_with_ids = [p for p in soup("p") if p.get("id")]
@@ -66,6 +71,7 @@ url = ("https://www.house.gov/representatives")
 html = requests.get(url).text
 soup = BeautifulSoup(html, "html5lib")
 
+# View in source code to get the links of website
 all_urls = [a["href"]
             for a in soup("a") if a.has_attr('href')]
 
@@ -110,6 +116,90 @@ assert not re.match(regex, "https://barky.house.gov/data")
 good_urls = [url for url in all_urls if re.match(regex, url)]
 
 print(len(good_urls)) # Size: 872
+#print(good_urls)
+
+good_urls = list(set(good_urls))
+print(len(good_urls))
+
 print(good_urls)
 
+# Retrieve press release of specific house
+jayapal_house_url = ("https://stivers.house.gov/")
+html = requests.get(jayapal_house_url).text
+soup = BeautifulSoup(html, "html5lib")
 
+links = [a["href"] for a in soup("a") if "press release" in a.text.lower()]
+
+# Better use a set to avoid duplicates
+links = {a["href"] for a in soup("a") if "press release" in a.text.lower()}
+
+# Notice that this is a relative link, which means we need to remember the originating site
+print(links)
+
+from typing import Dict, Set
+
+press_releases: Dict[str, Set[str]] = {}
+for house_url in good_urls:
+    html = requests.get(house_url).text
+    soup = BeautifulSoup(html, 'html5lib')
+    pr_links = {a['href'] for a in soup('a') if 'press releases' in a.text.lower()}
+    print(f"{house_url}: {pr_links}")
+    press_releases[house_url] = pr_links
+
+
+# We need to find which congresspeople have press release mentioning data
+
+
+def paragraph_mentions(text: str, keyword: str) -> bool:
+    """
+    Returns True if a <p> inside the text mentions {keyword}
+
+    Parameters
+    ----------
+    text : str
+        DESCRIPTION.
+    keyword : str
+        DESCRIPTION.
+
+    Returns
+    -------
+    bool
+        DESCRIPTION.
+
+    """
+    
+    soup = BeautifulSoup(text, "html5lib")
+    paragraphs = [p.get_text() for p in soup("p")]
+    
+    return any(keyword.lower() in paragraph.lower() for paragraph in paragraphs)
+
+text = """<body><h1>Facebook</h1><p>Twitter</p></body>"""
+assert paragraph_mentions(text, "twitter")
+assert not paragraph_mentions(text, "facebook")
+
+for house_url, pr_links in press_releases.items():
+    for pr_link in pr_links:
+        url = f"{house_url}/{pr_link}"
+        text = requests.get(url).text
+        
+        if paragraph_mentions(text, "data"):
+            print(f"{house_url}")
+            break
+
+url = ("https://gomez.house.gov/news/documentquery.aspx?DocumentTypeID=27")
+text = requests.get(url).text
+soup = BeautifulSoup(text, "html5lib")
+
+paragraphs = [p.get_text() for p in soup('p')]
+
+
+url = ("https://jayapal.house.gov/media/press-releases/")
+text = requests.get(url).text
+soup = BeautifulSoup(text, "html5lib")
+
+paragraphs = [p.text for p in soup('blockquote')]
+
+for paragraph in paragraphs:
+    if "data" in paragraph.lower():
+        print(True)
+        break
